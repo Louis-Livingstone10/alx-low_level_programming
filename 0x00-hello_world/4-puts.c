e1fa51704f708516944b666cb558d22368611c6d
Louis Livingstone <mogullivingstone@gmail.com>
2022-12-09 04:09:12 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,10 @@
 /**                                                                                                                                     
 * main - Entry                                                                                                                         
  * Return: Always 0                                                                                                                     
- */                                                                                                                                     
+ */                  
+
 int main(void)                                                                                                                          
 {                                                                                                                                       
 	        puts("\"Programming is like building a multi lingual puzzle");                                                            		return (0);
+
 }
