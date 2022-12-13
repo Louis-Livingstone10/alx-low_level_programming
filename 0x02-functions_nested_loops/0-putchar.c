0876d8114c5a2f587733e41c8aaa3b21f3295847
Louis Livingstone <mogullivingstone@gmail.com>
2022-12-13 10:12:30 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,6 +5,7 @@
  *Return: Always 0
  */
 int main(void)
+
 {
 	_putchar('_');
 	_putchar('p');
@@ -14,6 +15,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
